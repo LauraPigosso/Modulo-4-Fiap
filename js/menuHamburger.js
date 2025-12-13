a93541639ad455d8b20df138cb5905ce8b1d4645
felipe-071 <felipefelxg@gmail.com>
2025-12-12 23:09:17 -0300

    Adicionar lógica do header mobile

--- js/menuHamburger.js
+++ js/menuHamburger.js
@@ -1,3 +1,12 @@
 const menuHamburger = document.getElementById("menu-hamburger")
-
 const menuMobileAberto = document.getElementById("menu-mobile-aberto")
+const fecharMenuMobile = document.getElementById("menu-mobile__fechar")
+
+menuHamburger.addEventListener("click", () => {
+    menuMobileAberto.classList.add("ativo")
+})
+
+fecharMenuMobile.addEventListener("click", () => {
+    menuMobileAberto.classList.remove("ativo")
+})
+
